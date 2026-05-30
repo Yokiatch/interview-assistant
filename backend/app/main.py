e5ca91a4f35df4bf6c01a8af4c184c785932e748
Dinesh <dkkhatana136@gmail.com>
2026-05-30 22:40:36 +0530

    Redirect backend to vercel url

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title=settings.app_name)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://interview-assistant-azure.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
